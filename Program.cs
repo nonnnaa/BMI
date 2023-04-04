551112bab168650a49d03ec9ecef20794cb7d94b
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-04 11:06:51 +0700

    BMI

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            double height = Convert.ToDouble(Console.ReadLine());
+            // height (m)
+            double weight = Convert.ToDouble(Console.ReadLine());
+            // weight (kg)
+            double Bmi = Math.Round(weight / (Math.Pow(height, 2)), 1);
+            //Console.WriteLine(Bmi);
+            if(Bmi < 18)
+            {
+                Console.WriteLine("Underweight");
+            }
+            else if(Bmi < 25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if(Bmi < 30)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Obese");
+            } 
+        }
+    }
+}
